507e00d5012706edb769d1975bfbd80812ae82f9
JongGeun <dldvk9999@naver.com>
2024-08-17 20:24:26 +0900

    feature: vCheckbox 기능 추가

--- src/vCheckbox/index.tsx
+++ src/vCheckbox/index.tsx
@@ -1,5 +1,81 @@
 import React from 'react';
+import styles from './index.module.css';
+import { VCHECKBOX_PARAMS } from './vCheckbox';
 
-export function VCheckbox() {
-  return <input type="checkbox" />;
+export function VCheckbox({
+  vState = false,
+  vType = 'bottom',
+  vClassName = '',
+  vLabelClassName = '',
+  vCheckedClassName = '',
+  vCheckList = [],
+  vCheckedList = [],
+  vShowMessage = false,
+  vMessage = '',
+  vMessageClass = '',
+  vIsAnimate = false,
+  props = {},
+}: VCHECKBOX_PARAMS) {
+  // 체크박스 리스트 생성 함수
+  function makeCheckList() {
+    const result: React.JSX.Element[] = [];
+    vCheckList.forEach((val, i) => {
+      result.push(
+        <label
+          htmlFor={`vCheckbox-${i}`}
+          className={`${vLabelClassName} ${
+            vCheckedList[i] ? vCheckedClassName : ''
+          } ${vState ? styles.invalid : ''}`}
+          key={i}
+        >
+          <input
+            id={`vCheckbox-${i}`}
+            type="checkbox"
+            className={vClassName}
+            hidden
+            value={val}
+            checked={vCheckedList[i]}
+            {...props}
+          />
+          <span>{val}</span>
+        </label>
+      );
+    });
+    return <div className={styles.checkbox_layout}>{result}</div>;
+  }
+
+  switch (vType) {
+    case 'top':
+      return (
+        <div className={styles.flex_column}>
+          {vState && vShowMessage ? (
+            <p
+              className={`${vMessageClass} ${
+                vIsAnimate ? styles.animateMessage : ''
+              }`}
+            >
+              {vMessage}
+            </p>
+          ) : null}
+          {makeCheckList()}
+        </div>
+      );
+    case 'bottom':
+      return (
+        <div className={styles.flex_column}>
+          {makeCheckList()}
+          {vState && vShowMessage ? (
+            <p
+              className={`${vMessageClass} ${
+                vIsAnimate ? styles.animateMessage : ''
+              }`}
+            >
+              {vMessage}
+            </p>
+          ) : null}
+        </div>
+      );
+    default:
+      return <></>;
+  }
 }
---
+++ src/vCheckbox/vCheckbox.ts
@@ -0,0 +1,18 @@
+import propsType from '../types/vprops';
+
+// VCheclbox 파라미터
+export interface VCHECKBOX_PARAMS {
+  readonly vState: boolean; // 유효성 상태 값
+  readonly vType: // 유효성 메시지를 출력할 타입
+  'top' | 'bottom';
+  vClassName?: string; // 유효성 입힐 class 명
+  vLabelClassName?: string; // label class 명
+  vCheckedClassName?: string; // checked 되었을 때 class 명
+  vCheckList: string[]; // checkbox 리스트
+  vCheckedList: boolean[]; // 현재 체크되어있는 리스트 (vCheckList와 길이가 같아야하며 같은 인덱스마자 매핑)
+  readonly vShowMessage: boolean; // 유효성 메시지 출력할지
+  vMessage?: string; // 유효성 메시지
+  vMessageClass?: string; // 유효성 메시지에 입힐 class 명
+  vIsAnimate?: boolean; // 유효성 메시지 출력 시 애니메이션 적용할지
+  props?: propsType; // 기타 옵션
+}
