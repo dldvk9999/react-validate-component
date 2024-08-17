83f8a666b3ade90887fce30851c4ec08737f6702
JongGeun <dldvk9999@naver.com>
2024-08-17 20:40:37 +0900

    Test: VCheckbox test 생성

---
+++ test/vcheckbox.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { VCheckbox } from '../dist';
+
+describe('VCheckbox', () => {
+  it('renders without crashing', () => {
+    const input = document.createElement('input');
+    const root = createRoot(input);
+    root.render(
+      <VCheckbox
+        vState={true}
+        vType={'bottom'}
+        vClassName={'test'}
+        vLabelClassName={'test'}
+        vCheckedClassName={'test'}
+        vCheckList={[]}
+        vCheckedList={[]}
+        vShowMessage={true}
+        vMessage={'test'}
+        vMessageClass={'test'}
+        vIsAnimate={true}
+        props={{
+          onChange: () => {},
+        }}
+      />
+    );
+  });
+});
