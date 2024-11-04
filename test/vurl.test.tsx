dd5ca5ac299a207e3474ae0bea7f99f903d682fb
JongGeun <dldvk9999@naver.com>
2024-11-04 22:13:50 +0900

    test: vURL 테스트 추가

---
+++ test/vurl.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { VURL } from '../dist';
+
+describe('VText', () => {
+  it('renders without crashing', () => {
+    const input = document.createElement('input');
+    const root = createRoot(input);
+    root.render(
+      <VURL
+        vState={true}
+        vType={'outer'}
+        vClassName={'test'}
+        vShowMessage={true}
+        vMessage={'test'}
+        vLocateMessage={'bottom-left'}
+        vMessageClass={'test'}
+        vIsAnimate={true}
+        props={{
+          onChange: () => {},
+          placeholder: 'this is react-validate-component test.',
+          className: 'test',
+          defaultValue: 'test',
+        }}
+      />
+    );
+  });
+});
