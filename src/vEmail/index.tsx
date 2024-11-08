315e332b2e0086a218ff5290e7faf275a2ed20a2
JongGeun <dldvk9999@naver.com>
2024-11-08 23:43:27 +0900

    refactor: css 파일 단일화

--- src/vEmail/index.tsx
+++ src/vEmail/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './index.module.css';
+import styles from '../index.module.css';
 import { VEMAIL_PARAMS } from './vEmail';
 
 export function VEmail({
--- src/vText/index.tsx
+++ src/vText/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './index.module.css';
+import styles from '../index.module.css';
 import { VTEXT_PARAMS } from './vText';
 
 // Text
