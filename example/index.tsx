f7a0d3118e506cde6eb34b1ec345a5ab42fcb0c1
JongGeun <dldvk9999@naver.com>
2026-02-04 21:33:58 +0900

    fix: 홈페이지 디자인 및 성능 개선

--- example/index.tsx
+++ example/index.tsx
@@ -11,7 +11,6 @@ import Footer from './component/Footer/footer';
 import Body from './component/Body/body';
 import Common from './component/Nav/common';
 import Aurora from './component/Aurora/Aurora';
-import SplashCursor from './component/SplashCursor/SplashCursor';
 // Types
 import { settingsType } from './types';
 // Languages
@@ -76,7 +75,6 @@ const App = () => {
 
   return (
     <IntlProvider locale={locale} messages={flattedMessages}>
-      <SplashCursor DYE_RESOLUTION={800} />
       <main>
         <header ref={headerArea} className={styles.header}>
           <Aurora colorStops={['#3A29FF', '#FF94B4', '#FF3232']} speed={0.5} />
